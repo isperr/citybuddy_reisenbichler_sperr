47713860e104e340d24816513a409a7fb776ac0b
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2018-07-01 18:05:37 +0200

    chore: add image upload logic

--- app/src/main/java/com/example/citybuddy/RegisterActivity.java
+++ app/src/main/java/com/example/citybuddy/RegisterActivity.java
@@ -1,7 +1,12 @@
 package com.example.citybuddy;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +15,9 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,10 +31,15 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class RegisterActivity extends AppCompatActivity {
     DatePicker dpDate;
@@ -39,6 +51,11 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseStorage storage = FirebaseStorage.getInstance();
     StorageReference storageRef = storage.getReference();
 
+    ImageView imageView;
+    ImageView btnChoose;
+    Button btnUpload;
+    Uri filePath;
+    final int PICK_IMAGE_REQUEST = 71;
 
     public void submitLogin(View v){
         //Views & Stringbuilder
@@ -152,9 +169,72 @@ public class RegisterActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void uploadProfileImage(View v){
 
-        StorageReference profileRef = storageRef.child("profile_images/user");
+
+    private void uploadProfileImage(View v){
+        //StorageReference profileRef = storageRef.child("profile_images/user");
+        //makeToast("Hey");
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void testMe(View v){
+        //StorageReference avatar = storageRef.child("profile_images/avatar.png");
+        //makeToast(avatar.getBucket());
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageRef.child("profile_images/isabella.sperr@gmail.com");
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            makeToast("Uploaded");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            makeToast("Failed " + e.getMessage());
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+
+
+
     }
 
     @Override
@@ -179,6 +259,24 @@ public class RegisterActivity extends AppCompatActivity {
         dpDate.init(2000, 0, 1, null);
 
         mAuth = FirebaseAuth.getInstance();
+
+        imageView = findViewById(R.id.image_test);
+        btnChoose = findViewById(R.id.image_upload);
+        btnUpload = findViewById(R.id.upload_me);
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadProfileImage(v);
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testMe(v);
+            }
+        });
     }
 
     //Small country autocomplete
